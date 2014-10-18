4fb656b1d1ef7bdbdea73cf7b8e0206da43a0adc
michail <michailgames@yahoo.pl>
2014-10-19 01:01:54 +0200

    validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,9 @@
 class Team < ActiveRecord::Base
+  validates :name, :members, :leader, :contact_email, presence: true
+  validates :members, numericality: { only_integer: true,
+      less_than_or_equal_to: 16, greater_than_or_equal_to: 1,
+      message: "must be a number between 1 and 16" }
+  validates :name, :leader, length: { maximum: 80 }
+  validates_format_of :contact_email, :with =>
+      /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 end
